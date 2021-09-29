2f85f74a3744d02decaed56c0574bd4172ade1f4
Tony Liou <ltony1024@yahoo.com.tw>
2021-09-29 18:23:16 +0800

    Add gitignore and adjust skip methods

--- skipper.ts
+++ skipper.ts
@@ -1,15 +1,22 @@
 (async function () {
   const classNameList = ['ytp-ad-skip-button ytp-button', 'ytp-ad-overlay-close-button'];
 
+  let observedSkipBtn: Element | undefined;
+  let skipBtnObserver: MutationObserver | undefined;
+
   function getElements(classNames: string[]): Element[] {
     return classNames
-      .map(name => Array.from(document.getElementsByClassName(name)) || [])
+      .map(name => Array.from(document.getElementsByClassName(name)))
       .reduce((acc, elems) => acc.concat(elems), []);
   }
 
   function getAndClickButtons() {
     getElements(classNameList).forEach(button => {
-      triggerClick(button);
+      if (!isVisible(button as HTMLElement)) {
+        observeButton(button);
+      } else {
+        triggerClick(button);
+      }
     });
   }
 
@@ -20,6 +27,64 @@
 
     const evt = new MouseEvent('click', {bubbles: true, cancelable: false});
     el.dispatchEvent(evt);
+  }
+
+  /**
+   * Check the `display: none`
+   *
+   * @param el - The element you want to check
+   *
+   * @return `true` if it is invisible on the screen; otherwise, `false`
+   */
+  function isVisible(el: HTMLElement): boolean {
+    return el.offsetParent !== null;
+  }
+
+  /**
+   * Create a new MutationObserver to monitor the button.
+   * If it is visible, it will be clicked.
+   *
+   * @param button - The element need to be observed
+   */
+  function observeButton(button: Element) {
+    if (button === observedSkipBtn) {
+      return;
+    }
+
+    let parentWithDisplayStyle: HTMLElement | undefined;
+    for (let curParent: HTMLElement | null = button as HTMLElement; curParent !== null; curParent = curParent.parentElement) {
+      if (curParent.style.display === 'none') {
+        parentWithDisplayStyle = curParent;
+        break;
+      }
+    }
+
+    if (parentWithDisplayStyle === undefined) {
+      return;
+    }
+
+    if (skipBtnObserver && observedSkipBtn) {
+      skipBtnObserver.disconnect();
+      triggerClick(observedSkipBtn);
+    } else if (!skipBtnObserver) {
+      skipBtnObserver = new MutationObserver(() => {
+        if (observedSkipBtn !== undefined && !isVisible(observedSkipBtn as HTMLElement)) {
+          return;
+        }
+
+        if (observedSkipBtn !== undefined) {
+          triggerClick(observedSkipBtn);
+        }
+        observedSkipBtn = undefined;
+        if (skipBtnObserver !== undefined) {
+          skipBtnObserver.disconnect();
+        }
+      });
+    }
+
+    observedSkipBtn = button;
+
+    skipBtnObserver.observe(parentWithDisplayStyle, {attributes: true});
   }
 
   /**
@@ -43,7 +108,12 @@
     return new Promise(r => setTimeout(r, ms));
   }
 
-  for (let failCount = 0; !initObserver() && failCount < 5; failCount++) {
+  // entry
+  for (let failCount = 0; failCount < 5; failCount++) {
+    if (initObserver()) {
+      break;
+    }
+
     await sleep(1000);
   }
 })();
